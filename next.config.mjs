4b7b7d2cbe69493047eda34e2df0d89476905523
art-litv <artem15052003@gmail.com>
2023-02-04 15:24:51 +0200

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -9,8 +9,6 @@ const config = {
   },
   env: {
     SUPABASE_URL: "https://rheupcwqqudhdkavfpvd.supabase.co",
-    SUPABASE_KEY:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoZXVwY3dxcXVkaGRrYXZmcHZkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3NTUwOTIxOCwiZXhwIjoxOTkxMDg1MjE4fQ.SYYVffKhRO9pjxPMWn9Tq7JMQY1wNKTj-AJthK9iWec",
   },
 };
 export default config;
